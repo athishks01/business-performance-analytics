cbb4dd725843e0a1dce84c85693efb44fefdf14c
athishks01 <athishhlelesh@gmail.com>
2026-03-03 17:52:25 +0530

    Create schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS business_analytics;
+CREATE DATABASE business_analytics;
+USE business_analytics;
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    region VARCHAR(50),
+    signup_date DATE
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
